33060897bb4ec905bf519f1cf5999eafdd108c3e
Michel Bongard <michel_bongard@hotmail.com>
2019-02-25 15:55:22 +0100

    everything

---
+++ src/main/java/ch/hsr/ifs/persistence/ActiveRecord.java
@@ -0,0 +1,17 @@
+package persistence;
+
+public interface ActiveRecord{
+	
+	public static final int NOTINDB = -1;
+
+	public boolean save();
+	
+	public boolean delete();
+	
+	public int getID();
+	
+	public boolean isInDB();
+	
+	public boolean equals(Object obj);
+	
+}
